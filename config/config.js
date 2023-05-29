40c34b35a1d0215f8eb27a14da5501d70d08e433
Beerendan <brendanrbowen@gmail.com>
2023-05-29 19:40:23 -0400

    jawsdb test

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+const sequelize = new Sequelize(process.env.JAWSDB_DATABASE, process.env.JAWSDB_USERNAME, process.env.JAWSDB_PASSWORD, {
+    host: process.env.JAWSDB_HOST,
+    port: process.env.JAWSDB_PORT,
+    dialect: 'mysql',
+})
+
 module.exports = {
     HOST: process.env.HOST,
     USER: 'root',

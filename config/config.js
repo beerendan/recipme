1f5718a03d408364ca2b3011f5ce630ec4a0c4b2
Beerendan <brendanrbowen@gmail.com>
2023-05-29 20:02:13 -0400

    more jawsdb

--- config/config.js
+++ config/config.js
@@ -1,12 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-
 module.exports = {
     HOST: process.env.HOST,
     USER: 'root',
@@ -19,4 +13,22 @@ module.exports = {
         acquire: 30000,
         idle: 10000
     }
-}};
+};
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306,
+        }
+    );
+}
+
+module.exports = sequelize;
